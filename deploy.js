4d32123af543a7cb8f3a434ee2409b4368fe4457
timkim <timk@adobe.com>
2025-12-09 12:43:44 -0800

    Shorten delay

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
-// Utility function to add a 1 second delay
-const delay = (ms = 1000) => new Promise(resolve => setTimeout(resolve, ms));
+// Utility function to add a 300 millisecond delay
+const delay = (ms = 300) => new Promise(resolve => setTimeout(resolve, ms));
 
 // Utility function to process arrays in batches
 const processBatch = async (array, batchSize, processFn) => {
